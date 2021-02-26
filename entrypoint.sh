ae739532ad62954c4bdb29017a2f44189bfe5937
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-26 10:43:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 echo KtLint version: "$(ktlint --version)"
 
 ktlint --color --reporter=checkstyle "android/**/*.kt*" \
-  | tee (reviewdog -f=checkstyle -name="ktlint" -reporter=github-pr-review -fail-on-error=true)
+  | tee >("reviewdog -f=checkstyle -name=ktlint -reporter=github-pr-review -fail-on-error=true")
